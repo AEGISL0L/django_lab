54f2da525e0b4d593b56046f4bc1b3baa891f02c
juav <juav@localhost.localdomain>
2025-12-26 17:44:06 +0100

    added migrations

--- apps/formulations/apps.py
+++ apps/formulations/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class FormulationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'formulations'
+    name = 'apps.formulations'
+
+
